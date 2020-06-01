fe17e606c058ebd489d50ede44238c9580190642
Brandon Wong <Brandon.wongg@gmail.com>
2020-06-01 00:06:54 -0700

    added VueJS front end

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const request = require('request');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 app.listen(3000,function(){
     console.log('Connected to Server')
@@ -12,34 +14,38 @@ app.listen(3000,function(){
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
 
 //app.use('/',function(req,res){
 //    res.send('Weather Application')
 //});
 
-app.get('/WeatherApp',function(req,res){
-    res.sendFile(__dirname+'/HTML/Front.html')
+//app.get('/',function(req,res){
+//    res.send(response);
 //    const City = new User(req.body);
 //    const JString = JSON.stringify(City);
 //    const cityFinal = JString.match(/\y":(.*?)\}/i)[1];
 //    console.log(cityFinal)
-});
+//});
 
 const API = process.env.apiKey;
 
 app.post("/WeatherApp",function(req,res){
     let city = (req.body.City)
+//    let city = "Reno"
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${API}`
-    console.log(city)
+    console.log(city);
 
     request(url, function(err,response,body) {
         if(err){
-            res.sendFile(__dirname+'/HTML/Front.html');
+            res.send(err);
         }   
         else {
-            let weather = JSON.parse(body)
-            let message = `It's ${weather.main.temp} degrees in ${weather.name}`;
-            res.send(message);
+//            let weather = JSON.parse(body)
+//            let message = `It's ${weather.main.temp} degrees in ${weather.name}`;
+            res.send(body);
+            console.log(body);
         }
-    }); 
+//        res.status(201).send();
+    });
 });
